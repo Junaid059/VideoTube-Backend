6472539de55181655651b0761a1662f877b771fb
junaid khalid <junaidkhalid2001@gmail.com>
2024-10-04 20:48:14 +0500

    user model

---
+++ src/models/user.models.js
@@ -0,0 +1,56 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    fullname: {
+      type: String,
+      required: true,
+    },
+
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+
+    coverImage: {
+      type: String, //cloudinary url
+    },
+
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      unique: true,
+    },
+
+    refreshTokens: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
